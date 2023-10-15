1f54c1ea1fd96f0f0864e896cbc4a5cf23026afb
William Cáceres <willycaceres2002@gmail.com>
2023-10-15 14:20:10 -0600

    API documentation agregada con apicore

--- inventario_api/settings.py
+++ inventario_api/settings.py
@@ -135,3 +135,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#configuracion adicional para la documentacion de la API
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
+}
--- inventario_api/urls.py
+++ inventario_api/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('api.urls'))
+    path('', include('api.urls')),
+    #ruta para documentacion
+    path('docs/', include_docs_urls(title='API Documentation'))
+
 ]
