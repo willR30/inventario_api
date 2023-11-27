a79835418af0f532edccaa7d1d7dbc21fd97c98f
William Cáceres <willycaceres2002@gmail.com>
2023-11-27 00:15:07 -0600

    get customer in the invoice list funtion

--- api/serializers.py
+++ api/serializers.py
@@ -63,6 +63,7 @@ class SaleSerializer(serializers.ModelSerializer):
 
 
 class InvoiceSerializer(serializers.ModelSerializer):
+    customer = CustomerSerializer()  # Agregar el serializador del cliente
     sale = SaleSerializer(many=True, read_only=True)  # Utiliza el serializador de Sale para representar las ventas
 
     class Meta:
