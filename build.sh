92c4c9bbc0a01b3c724046ea97ddca8826c0b1fe
willR30 <willycaceres2002@gmail.com>
2024-07-01 22:21:20 -0600

    set admin user

--- build.sh
+++ build.sh
@@ -20,7 +20,7 @@ import os
 import django
 from django.contrib.auth import get_user_model
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inventario_api.settings')
 django.setup()
 
 User = get_user_model()
