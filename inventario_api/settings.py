727d78b0d0bcf5fe1691efca1241ec2fbdcf9330
William Ballesteros <50780601+willR30@users.noreply.github.com>
2025-10-08 11:38:54 -0600

    cross headers

--- inventario_api/settings.py
+++ inventario_api/settings.py
@@ -65,6 +65,7 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -73,7 +74,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    
 ]
 
 ALLOWED_HOSTS = ['*']
